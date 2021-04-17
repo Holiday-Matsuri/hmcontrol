51ed82b4d71441495fc79338924bebb12f5f6e65
Andrew Pierson <andrewcpierson@gmail.com>
2021-04-17 13:39:20 -0400

    Added user association to events

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,6 @@
 class Event < ApplicationRecord
   belongs_to :convention
+  belongs_to :user
   extend FriendlyId
   friendly_id :event_name, use: :slugged
   enum status: [:submitted, :approved, :rejected, :scheduled, :waitlist]
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_17_013530) do
+ActiveRecord::Schema.define(version: 2021_04_17_172254) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,8 +37,10 @@ ActiveRecord::Schema.define(version: 2021_04_17_013530) do
     t.bigint "convention_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id", null: false
     t.index ["convention_id"], name: "index_events_on_convention_id"
     t.index ["slug"], name: "index_events_on_slug"
+    t.index ["user_id"], name: "index_events_on_user_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
@@ -70,4 +72,5 @@ ActiveRecord::Schema.define(version: 2021_04_17_013530) do
   end
 
   add_foreign_key "events", "conventions"
+  add_foreign_key "events", "users"
 end
