135ea8836399e83b7ca6f44658e3577aa4ddd056
Andrew Pierson <andrewcpierson@gmail.com>
2021-06-26 12:07:34 -0400

    sonarqube

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
     resources :events, only: [:index, :show]
     resources :event_applications
   end
-  resources :profiles, only: [:index, :update]
+  resources :profiles, path: 'profile', only: [:index, :update]
 end
