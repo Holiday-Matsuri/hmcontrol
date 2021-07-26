08cb1d40fcac93070cf7b95e49f35d5b3dbedffd
Andrew Pierson <andrewcpierson@gmail.com>
2021-07-25 20:15:09 -0400

    some stuff

--- app/controllers/admin/application_reviews_controller.rb
+++ app/controllers/admin/application_reviews_controller.rb
@@ -1,4 +1,4 @@
-class Admin::ApplicationReviewsController < ApplicationController
+class Admin::ApplicationReviewsController < Admin::AdminController
   before_action :authenticate_staff
   def index
     
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'static#root'
   namespace :admin do
+    resources :users
     resources :application_reviews, controller: :application_reviews
   end
   resources :conventions, only: [:index, :show] do
